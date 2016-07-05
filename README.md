32f669ba7b60610329d6ce6c16295a60bc48c465
Jai <jai@doselect.com>
2016-07-05 18:17:56 +0530

    [Fix #5] Add countup support
    Signed-off-by: Jai <jai@doselect.com>

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## ng-ticker
 
+Count up || Count down
+
 #### Usage
 
 ```
@@ -11,18 +13,31 @@ bower install ng-ticker
 - From the controller, pass the expiry (String) in ISO 8601 format (2016-02-05T17:00:00)
 
 ```
+// To count down
 $scope.expiry = '2016-02-05T17:00:00'
-$scope.ticker = ngTicker.ticker($scope.expiry, instanceIdentifier)
+$scope.ticker = ngTicker.tickDown($scope.expiry, instanceIdentifier)
+
+// To count up
+$scope.startTime = '2016-02-05T17:00:00'
+$scope.ticker = ngTicker.tickup($scope.startTime, instanceIdentifier)
 ```
 
-- Listen for `ngTicker:tick + instanceIdentifier` and `ngTicker:expired + instanceIdentifier` signals
+- For count down ticker, listen for `ngTicker:tick + instanceIdentifier` and `ngTicker:expired + instanceIdentifier` signals
+
+- For count up ticker, listen for `ngTicker:tick + instanceIdentifier` and `ngTicker:notStarted + instanceIdentifier` signals
 
 ```
 $scope.$on('ngTicker:tick + instanceIdentifier', function (event, tickMeta) {
   // {{hours : 02, minutes : 08, seconds : 10}} tickMeta
 })
 
+// Count down ticker
 $scope.$on('ngTicker:expired + instanceIdentifier', function (event, tickMeta) {
+  // {{hours : 00, minutes : 00, seconds : 00}} tickMeta
+})
+
+// Count up ticker
+$scope.$on('ngTicker:notStarted + instanceIdentifier', function (event, tickMeta) {
   // {{hours : 00, minutes : 00, seconds : 00}} tickMeta
 })
 ```
--- ng-ticker.js
+++ ng-ticker.js
@@ -1,9 +1,10 @@
+
 angular.module('ngTicker', [])
 
   .factory('ngTicker', ['$rootScope', '$interval', function ($rootScope, $interval) {
-    var tickerFunc, startTicker
     return {
-      ticker: function (expiry, tickerInstance) {
+      tickDown: function (expiry, tickerInstance) {
+        var tickerFunc, startTicker
         startTicker = function () {
           tickerFunc = $interval(function () {
             var formattedExpiry = moment(moment(expiry), moment.ISO_8601)
@@ -26,6 +27,38 @@ angular.module('ngTicker', [])
             if (JSON.stringify(tickMeta) === JSON.stringify({'hours': '00', 'minutes': '00', 'seconds': '00'})) {
               $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
               $rootScope.$broadcast('ngTicker:expired' + tickerInstance, tickMeta)
+              $interval.cancel(tickerFunc)
+            } else {
+              $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
+            }
+          }, 1000)
+        }
+
+        startTicker()
+      },
+
+      tickUp: function (startTime, tickerInstance) {
+        var tickerFunc, startTicker
+        startTicker = function () {
+          tickerFunc = $interval(function () {
+            var formattedExpiry = moment(moment(startTime), moment.ISO_8601)
+            var totalSeconds = moment().diff(moment(formattedExpiry), 'seconds')
+            var duration = moment.duration(totalSeconds + 1, 'seconds')
+
+            var hours = duration.hours()
+            var minutes = duration.minutes()
+            var seconds = duration.seconds()
+
+            var tickMeta = {
+              hours: (hours < 0 ? '00' : (hours < 10 ? '0' + hours : hours)),
+              minutes: (minutes < 0 ? '00' : (minutes < 10 ? '0' + minutes : minutes)),
+              seconds: (seconds < 0 ? '00' : (seconds < 10 ? '0' + seconds : seconds))
+            }
+
+            // Broadcast ticker events
+            if (JSON.stringify(tickMeta) === JSON.stringify({'hours': '00', 'minutes': '00', 'seconds': '00'})) {
+              $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
+              $rootScope.$broadcast('ngTicker:notStarted' + tickerInstance, tickMeta)
               $interval.cancel(tickerFunc)
             } else {
               $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
