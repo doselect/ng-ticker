4ba907b586294c3826d404d21c7804cd3409a38e
Jai <jai@doselect.com>
2016-08-10 20:29:25 +0530

    Add `days` to down ticker
    Signed-off-by: Jai <jai@doselect.com>

--- ng-ticker.js
+++ ng-ticker.js
@@ -10,6 +10,7 @@ angular.module('ngTicker', [])
             var formattedExpiry = moment(moment(expiry), moment.ISO_8601)
             var totalSeconds = moment(formattedExpiry).diff(moment(), 'seconds')
 
+            var days = Math.floor(totalSeconds / 86400)
             var hours = Math.floor(totalSeconds / 3600)
             var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60)
             var seconds = totalSeconds - (hours * 3600) - (minutes * 60)
@@ -18,13 +19,14 @@ angular.module('ngTicker', [])
             seconds = Math.round(seconds * 100) / 100
 
             var tickMeta = {
+              days: (days < 0 ? '00' : (days < 10 ? '0' + days : days)),
               hours: (hours < 0 ? '00' : (hours < 10 ? '0' + hours : hours)),
               minutes: (hours < 0 ? '00' : (minutes < 10 ? '0' + minutes : minutes)),
               seconds: (hours < 0 ? '00' : (seconds < 10 ? '0' + seconds : seconds)),
             }
 
             // Broadcast ticker events
-            if (JSON.stringify(tickMeta) === JSON.stringify({'hours': '00', 'minutes': '00', 'seconds': '00'})) {
+            if (JSON.stringify(tickMeta) === JSON.stringify({'days': '00', 'hours': '00', 'minutes': '00', 'seconds': '00'})) {
               $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
               $rootScope.$broadcast('ngTicker:expired' + tickerInstance, tickMeta)
               $interval.cancel(tickerFunc)
