d47eabb72da18160c4c18ed47f4d858b6ba04670
Arun Prajapati <arun@doselect.com>
2022-10-07 11:13:31 +0530

    [PF-1886] Change tickDown logic to calculate actual time.
    Signed-off-by: Arun Prajapati <arun@doselect.com>

--- ng-ticker.js
+++ ng-ticker.js
@@ -8,30 +8,25 @@ angular.module('ngTicker', [])
         startTicker = function () {
           tickerFunc = $interval(function () {
             var formattedExpiry = moment(moment(expiry), moment.ISO_8601)
-            var totalSeconds = moment(formattedExpiry).diff(moment(), 'seconds')
+            var duration = moment.duration(moment(formattedExpiry).diff(moment()))
 
-            var days = Math.floor(totalSeconds / 86400)
-	    var hours = Math.floor((totalSeconds - (days * 86400)) / 3600)
-	    var minutes = Math.floor((totalSeconds - (days * 86400) - (hours * 3600)) / 60)
-	    var seconds = totalSeconds - (days * 86400) - (hours * 3600) - (minutes * 60)
- 
-            // Round off seconds
-            seconds = Math.round(seconds * 100) / 100
+            var days = Math.floor(duration.asDays())
+            var hours = duration.hours()
+            var minutes = duration.minutes()
+            var seconds = duration.seconds()
 
             var tickMeta = {
               days: (days < 0 ? '00' : (days < 10 ? '0' + days : days)),
               hours: (hours < 0 ? '00' : (hours < 10 ? '0' + hours : hours)),
-              minutes: (hours < 0 ? '00' : (minutes < 10 ? '0' + minutes : minutes)),
-              seconds: (hours < 0 ? '00' : (seconds < 10 ? '0' + seconds : seconds)),
+              minutes: (minutes < 0 ? '00' : (minutes < 10 ? '0' + minutes : minutes)),
+              seconds: (seconds < 0 ? '00' : (seconds < 10 ? '0' + seconds : seconds)),
             }
 
             // Broadcast ticker events
-            if (JSON.stringify(tickMeta) === JSON.stringify({'days': '00', 'hours': '00', 'minutes': '00', 'seconds': '00'})) {
-              $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
+            $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
+            if (duration.asSeconds() <= 0) {
               $rootScope.$broadcast('ngTicker:expired' + tickerInstance, tickMeta)
               $interval.cancel(tickerFunc)
-            } else {
-              $rootScope.$broadcast('ngTicker:tick' + tickerInstance, tickMeta)
             }
           }, 1000)
         }
